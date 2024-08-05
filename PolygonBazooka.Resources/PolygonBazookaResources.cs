cba4794c808daa5b7b7b9779e7b095439f83289a
Blasty <79897422+BlastyTheDev@users.noreply.github.com>
2024-08-05 16:46:14 +1000

    Delete PolygonBazooka.Resources directory

--- PolygonBazooka.Resources/PolygonBazookaResources.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Reflection;
-
-namespace PolygonBazooka.Resources
-{
-    public static class PolygonBazookaResources
-    {
-        public static Assembly ResourceAssembly => typeof(PolygonBazookaResources).Assembly;
-    }
-}
